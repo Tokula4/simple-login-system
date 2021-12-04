99c5559d02c1974ecdf6154add16ac574778b8ec
Emmanuel Tokula <emmanueltokula03@gmail.com>
2021-12-04 12:56:40 +0100

    small touches

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
     path('home/', views.home, name='home'),
-    path( 'register/', views.registerUser, name='register'),
-    path('login/',views.loginUser, name='Login'),
+    path('register/',views.register,name='register'),
+    
+   
+   
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,18 @@
-from django.shortcuts import   render , redirect
-from django.contrib.auth import authenticate, login  
+from django.shortcuts import render
 from django.contrib import messages
-
-# Create your views here.
-from .form import RegisterForm
-
-def index(request):
-    return render(request , 'accounts/index.html', {})
-
-def registerUser(request):
-    form = RegisterForm()
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('Login')
-        else:
-            form = RegisterForm()
-    return render(request , 'accounts/register.html', {'form':form})
-
-def loginUser(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        if username and password :
-            user = authenticate(username = username , password = password)
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.error(request , 'username or password is incorrect')
-        else:
-             messages.error(request, 'fillout all the fields')
-    return render ( request, 'accounts/login.html',{})   
+from django.contrib.auth.forms import UserCreationForm
 def home(request):
-    return render(request, 'accounts/home.html', {}) 
+    return render(request,'accounts/home.html')
+
+
+# User Register
+def register(request):
+    Form = UserCreationForm
+    if request.method =='POST':
+        Form=UserCreationForm(request.POST)
+        if Form.is_valid():
+            Form.save()
+            messages.success(request,'User has been registered.')
+    return render(request,'accounts/register.html',{'form':Form})
 
 
